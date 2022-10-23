12ca1db849efbacf114c46c940d697f03dbd7fa8
tanachok12 <tanachok_d@elearning.ac.th>
2022-10-23 19:00:05 +0700

    first commit naja

---
+++ src/main/java/se331/rest/service/EventService.java
@@ -0,0 +1,17 @@
+package se331.rest.service;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import se331.rest.entity.Event;
+
+public interface EventService {
+    Integer getEventSize();
+
+    Page<Event> getEvents(Integer pageSize, Integer page);
+
+    Event getEvent(Long id);
+
+    Event save(Event event);
+    Page<Event> getEvents(String title, Pageable pageable);
+
+}
